b3d8e0c37da2a1906058de329b5572efd5913466
Pandu Dewabrata <73803874+dewabratapandu@users.noreply.github.com>
2021-04-05 11:59:49 +0700

    Add files via upload

---
+++ src/CascadeClassifiers.py
@@ -0,0 +1,176 @@
+import os
+import csv
+import time
+import numpy as np
+import cv2
+import src.adaboost as ada
+import src.haar_extractor as haar
+from tqdm import tqdm
+
+class CascadeClassifier:
+    def __init__(self):
+        self.cascadeClf = []
+
+    def save(self, filename):
+        # prepare .txt file
+        model = open(filename, "w")
+        model.write("")
+
+        # Write cascadeClf to .txt file
+        for strClf in self.cascadeClf:
+            for weakClf in strClf:
+                model.write(str(weakClf[0])); model.write(' ')
+                model.write(str(weakClf[1])); model.write(' ')
+                model.write(str(weakClf[2])); model.write(' ')
+                model.write(str(weakClf[3])); model.write(' ')
+                model.write(str(weakClf[4])); model.write(' ')
+                model.write(str(weakClf[5])); model.write(' ')
+                model.write(str(weakClf[6]))
+                model.write("\n")
+            model.write("end of cascade\n")
+        model.close()
+
+    def load(self, filename):
+        self.cascadeClf = []
+        strClf = []
+        with open(filename) as csvfile:
+            modeltxt = csv.reader(csvfile, delimiter=' ')
+            for line in modeltxt:
+                if(line[0] == 'end'):
+                    cp = strClf.copy()
+                    self.cascadeClf.append(cp)
+                    strClf.clear()
+                elif(line is None):
+                    continue
+                else:
+                    for i in range(1, 6):
+                        line[i] = int(line[i])
+                    line[6] = float(line[6])
+                    strClf.append(line)
+
+    def feature_extracting(self, P_path, N_path, imsize=(24,24), featureTypes = ("type_one", "type_two", "type_three", "type_four", "type_five"), step=1):
+        self.imsize = imsize
+        self.P_path = P_path
+        self.N_path = N_path
+        self.features = []
+
+        # Haar Feature Extraction
+        self.P_paths = [os.path.join(self.P_path, s) for s in sorted(os.listdir(self.P_path))]
+        self.N_paths = [os.path.join(self.N_path, s) for s in sorted(os.listdir(self.N_path))]
+        training_paths = self.N_paths + self.P_paths
+
+        print('Extracting Haar Feature...')
+        for i, filename in enumerate(tqdm(training_paths)):
+            im = cv2.imread(filename, 0)
+            im = cv2.resize(im, self.imsize)
+            
+            # Calculate integral image
+            im = haar.integralImage(im)
+            
+            # Calculate haar features
+            features, features_list = haar.getFeatures(im, featureTypes=featureTypes, step=step)
+            self.features.append(features)
+            if len(features_list) != 0:
+                self.features_list = features_list
+        
+            
+        print('Haar Extracted...')
+
+    def predict(self, src, cascadeClf=None, imsize=None):
+        if cascadeClf == None:
+            cascadeClf = self.cascadeClf
+        if imsize == None:
+            imsize = self.imsize
+        im = cv2.imread(src, 0)
+        im = cv2.resize(im, imsize)
+        im = haar.integralImage(im)
+        for strClf in cascadeClf:
+            ahx = 0 # sum of alfa * hx
+            sumAlfa = 0 # sum of alfa
+            for weakClf in strClf:
+                fx = haar.computeFeature(im, weakClf[0], weakClf[1], weakClf[2], weakClf[3], weakClf[4])
+                hx = 1 if fx < weakClf[5] else 0
+                alfa = float(weakClf[6])
+                ahx += alfa * hx
+                sumAlfa += alfa
+            result = 1 if ahx >= 0.5*sumAlfa else 0
+            if(result == 0):
+                break
+        return result
+
+    def fit(self, Ftarget=0.2, Dtarget=0.8, f=0.2, d=0.8, max_cascade=50):
+        F = np.ones(max_cascade, dtype=float)
+        D = np.ones(max_cascade, dtype=float)
+        remove_indices = []
+        i = 0 # cascade index
+
+        while F[i] > Ftarget or D[i] < Dtarget:
+            if(i >= max_cascade):
+                break
+            print('\nCascade :',i)
+
+            n = 0
+            i+=1
+            falserate = []; detrate = []
+            F[i] = F[i-1]; D[i] = D[i-1]
+            len_P = len(self.P_paths); len_N = len(self.N_paths)
+            thres = len_N + len_P
+            strClf = []
+            while F[i] > (F[i-1]*f) or D[i] < (D[i-1]*d):
+                start = time.time()
+                n += 1
+                print("\nNumber of feature (weak classfifier) :", n)
+
+                # Train adaboost
+                thres -= 1
+                if n == 1:
+                    prevCond, self.features, self.features_list = ada.adaboost(len_P, len_N, self.features, self.features_list, thres)
+                else:
+                    prevCond, self.features, self.features_list = ada.adaboost(len_P, len_N, self.features, self.features_list, thres, prevCond=prevCond)
+                strClf = prevCond[0]
+
+                # Evaluate F and D
+                clf = [strClf]
+                posResult = [self.predict(p, clf) for p in self.P_paths]
+                negResult = [self.predict(n, clf) for n in self.N_paths]
+                TP, FN, TN, FP = self.confusionMatrix(len_P, len_N, posResult, negResult)
+                print("TP", TP, "TN", TN, "FP", FP, "FN", FN)
+    
+                F[i] = FP/(FP+TN)
+                D[i] = TP/(TP+FN)
+                # falserate.append(FP/(FP+TN))
+                # detrate.append(TP/(TP+FN))
+                # F[i] = np.prod(falserate)
+                # D[i] = np.prod(detrate)
+                
+                print("F[i]", F[i], "   F[i-1]*f", F[i-1]*f)
+                print("D[i]", D[i], "   D[i-1]*d", D[i-1]*d)
+                print("Time :", time.time()-start)
+
+            if len(strClf) == 0:
+                break
+
+            # Append strong classifier to cascade classifier
+            self.cascadeClf.append(strClf)
+
+            # Leaving negative images that is wrongly detected (False Positive)
+            self.N_paths = self.eliminateTrueNeg(self.N_paths, negResult)
+            negResult = np.array(negResult)
+            remove_indices = np.argwhere(negResult == 0)
+            self.features = [i for j, i in enumerate(self.features) if j not in remove_indices]
+
+    def confusionMatrix(self, P, N, posResult, negResult):
+        TP = sum([1 for h in posResult if h==1])
+        FN = P - TP
+
+        TN = sum([1 for h in negResult if h==0])
+        FP = N - TN
+
+        return TP, FN, TN, FP
+
+    def eliminateTrueNeg(self, Nimg, prediction):
+        NimgBaru = list()
+        for i in range(len(prediction)):
+            if(prediction[i] == 1):
+                NimgBaru.append(Nimg[i])
+        return NimgBaru
---
+++ src/adaboost.py
@@ -0,0 +1,60 @@
+import numpy as np
+import math
+
+def adaboost(P, N, features, features_list, thres, prevCond=None):
+      
+    if prevCond is None:
+        strClf = []
+        # initialize weight for each positive and negative image in 1D array
+        weight = np.hstack((np.ones(N)*1/(2*N), np.ones(P)*1/(2*P)))
+    else:
+        strClf, weight = prevCond
+
+    # positif and negatif label in 1D array
+    label = np.hstack((np.zeros(N), np.ones(P)))
+    # number of images and features
+    num_images = len(features)
+    num_features = len(features[0])
+    # weight normalization
+    weight = weight / np.sum(weight)
+
+
+    # calculate hypothesis
+    f = np.array(features)
+    hipo = np.where(f < f[thres, :], 1, 0)
+
+
+    # calculate error
+    error_classification = np.sum(weight * np.abs(label-np.transpose(hipo)), axis=1)
+
+    
+    # best feature (feature with minimum error)
+    min_error_index = np.argmin(error_classification)
+    best_error = error_classification[min_error_index]
+    print('best error =', best_error)
+
+    # calculate alpha
+    a = alpha(best_error)
+
+    # weight update
+    b = beta(best_error)
+    weight = np.where(hipo[:,min_error_index]==label, weight*b, weight)
+
+    # save feature and alpha
+    best_feature = list(features_list[min_error_index])
+    best_feature.extend([features[thres][min_error_index], a])
+    strClf.append(best_feature)
+
+    # remove best feature from features list
+    for i in range(num_images):
+        features[i].pop(min_error_index)
+    features_list.pop(min_error_index)
+
+    prevCond = (strClf, weight)
+    return prevCond, features, features_list
+
+def alpha(error):
+    return math.log(1/beta(error))
+
+def beta(error): 
+    return error/(1 - error) 

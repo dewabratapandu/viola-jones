2ad9a1aae810c3fb32d8dfe3e75c3dfe7e54b051
Pandu Dewabrata <73803874+dewabratapandu@users.noreply.github.com>
2021-04-05 11:56:19 +0700

    Update training.py

--- training.py
+++ training.py
@@ -1,18 +1,5 @@
 from src.CascadeClassifiers import CascadeClassifier
 
-## DATASET GEDUNG REKTORAT UB
-# model_path = "model_ub.txt"
-# P_paths = 'training_ub/true'
-# N_paths = 'training_ub/false'
-# imsize = (25,50)
-
-## DATASET MATA KATARAK
-# model_path = "model_katarak.txt"
-# P_paths = 'training_katarak/true'
-# N_paths = 'training_katarak/false'
-# imsize = (24, 24)
-
-
 ## DATASET FACE
 model_path = "model_face.txt"
 P_paths = '/media/pandu/Dewabrata/natural images/face'

73e1454b5402ed5642c4e84cff8ab89b08354919
Pandu Dewabrata <73803874+dewabratapandu@users.noreply.github.com>
2021-04-05 11:55:48 +0700

    Update training.py

--- training.py
+++ training.py
@@ -7,19 +7,19 @@ from src.CascadeClassifiers import CascadeClassifier
 # imsize = (25,50)
 
 ## DATASET MATA KATARAK
-model_path = "model_katarak.txt"
-P_paths = 'training_katarak/true'
-N_paths = 'training_katarak/false'
-imsize = (24, 24)
+# model_path = "model_katarak.txt"
+# P_paths = 'training_katarak/true'
+# N_paths = 'training_katarak/false'
+# imsize = (24, 24)
 
 
 ## DATASET FACE
-# model_path = "model_face.txt"
-# P_paths = '/media/pandu/Dewabrata/natural images/face'
-# N_paths = '/media/pandu/Dewabrata/natural images/non-face'
-# imsize = (24,24)
+model_path = "model_face.txt"
+P_paths = '/media/pandu/Dewabrata/natural images/face'
+N_paths = '/media/pandu/Dewabrata/natural images/non-face'
+imsize = (24,24)
 
 model = CascadeClassifier()
-model.feature_extracting(P_paths, N_paths, step=2)
-model.fit(f=0.3, d=0.7)
+model.feature_extracting(P_paths, N_paths)
+model.fit(Ftarget=0.2, Dtarget=0.8, f=0.2, d=0.8)
 model.save(model_path)

85663f8a4122237b450fd5395ca0dc88d6b5f7b9
Pandu Dewabrata <73803874+dewabratapandu@users.noreply.github.com>
2021-04-18 16:44:28 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,8 +1,9 @@
 import cv2
 import os
+import numpy as np
 from src.CascadeClassifiers import CascadeClassifier
-import src.haar_extractor as haar
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 def test(model_path, P_paths, N_paths, imsize):
     # LOAD MODEL
@@ -13,7 +14,9 @@ def test(model_path, P_paths, N_paths, imsize):
 
     posResult = [model.predict(cv2.imread(p,0), imsize=imsize) for p in P_paths]
     negResult = [model.predict(cv2.imread(n,0), imsize=imsize) for n in N_paths]
-    TP, FN, TN, FP = model.confusionMatrix(len_P, len_N, posResult, negResult)
+    y_true = np.hstack((np.zeros(len_N), np.ones(len_P)))
+    y_pred = np.array(negResult + posResult)
+    TN, FP, FN, TP = confusion_matrix(y_true, y_pred, labels=[1,0]).ravel()
 
     print("\nTP", TP, "TN", TN, "FP", FP, "FN", FN)
     print("Accuracy :", (TP+TN)/(TP+TN+FP+FN))
@@ -21,8 +24,8 @@ def test(model_path, P_paths, N_paths, imsize):
 
 ## DATASET
 model_path = "model.txt"
-P_path = 'Dataset/face'
-N_path = 'Dataset/non-face'
+P_path = 'dataset/face'
+N_path = 'dataset/non-face'
 imsize = (24, 24)
 
 P_paths = [os.path.join(P_path, s) for s in sorted(os.listdir(P_path))]
@@ -32,7 +35,7 @@ N_train, N_test = train_test_split(N_paths, test_size=0.2, shuffle=False)
 
 model = CascadeClassifier()
 model.feature_extracting(P_train, N_train)
-model.fit(Ftarget=0.1, Dtarget=0.9, f=0.2, d=0.8)
+model.fit(Ftarget=0.1, Dtarget=0.9, f=0.1, d=0.9)
 model.save(model_path)
 
 print('TESTING...')
